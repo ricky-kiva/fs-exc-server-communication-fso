4920a2c4e977977c571c2705b0f0bae357427bed
ricky-kiva <ricky.kiva@gmail.com>
2023-12-14 23:56:17 +0700

    feat(app-src): impl. render multiple courses

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,5 +1,5 @@
 const Header = ({ name }) => 
-  <h1>{name}</h1>
+  <h2>{name}</h2>
 
 const Content = ({ parts }) =>
   <div>
@@ -20,37 +20,61 @@ const Course = ({ course }) => {
     <div>
       <Header name={name} />
       <Content parts={parts} />
-      <p><b>total of {total} exercises</b></p>
+      <h3>total of {total} exercises</h3>
     </div>
   )
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      }, {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      }, {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }, {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        }, {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        }, {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        }, {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        }, {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      {courses.map(course =>
+        <Course key={course.id} course={course} />
+      )}
+    </div>
+    
+  )
 }
 
 export default App
