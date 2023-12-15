e655896cab480d02b618d327e129f657c32ade72
ricky-kiva <ricky.kiva@gmail.com>
2023-12-15 11:03:51 +0700

    feat(src-app): impl. alert on duplicate `persons`

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,6 +1,48 @@
+import { useState } from "react"
+
 const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const personObject = { name: newName }
+
+    const duplicatePerson = persons.find(person => person.name === personObject.name)
+    const isDuplicate = JSON.stringify(personObject) === JSON.stringify(duplicatePerson)
+
+    isDuplicate 
+      ? alert(`${newName} is already added to phonebook`)
+      : setPersons(persons.concat(personObject))
+
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => setNewName(event.target.value)
+
   return (
-    <div>The spirits carries on</div>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: 
+          <input
+            value={newName} 
+            onChange={handleNameChange} 
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person, i) =>
+          <div key={i}>{person.name}</div>  
+        )}
+      </div>
+    </div>
   )
 }
 
