57f39c25544fdc47888c68a22f5290b21e920615
ricky-kiva <ricky.kiva@gmail.com>
2023-12-16 23:02:01 +0700

    feat(src-app): impl. change handler for found countries

--- countries/src/App.jsx
+++ countries/src/App.jsx
@@ -1,15 +1,27 @@
 import { useEffect, useState } from "react"
-import Country from "./components/Country"
 import countryService from "./services/countries"
 
 
 const App = () => {
   const [countries, setCountries] = useState([])
+  const [found, setFound] = useState([])
 
   useEffect(() => {
     countryService.getAll()
       .then(initialCountries => setCountries(initialCountries))
   }, [])
+
+  const foundChangeHandler = (event) => {
+    const text = event.target.value
+    if (text.length === 0) {
+      setFound([])
+    } else {
+      setFound(countries.filter(c => 
+        c.name.common.toLowerCase()
+          .includes(text.toLowerCase())
+      ))
+    } 
+  }
 
   if (countries.length === 0) {
     return <div>Loading..</div>
@@ -17,7 +29,12 @@ const App = () => {
 
   return (
     <div>
-      <Country country={countries[0]}/>
+      find countries <input onChange={foundChangeHandler} />
+      <ul>
+        {found.map((c, i) => 
+          <li key={i}>{c.name.common}</li>
+        )}
+      </ul>
     </div>
   )
 }
